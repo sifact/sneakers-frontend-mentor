f51568062948a753d4f22deb35d5bdf3cfc28b34
sifat <sifatami596@gmail.com>
2023-06-19 02:30:05 -0700

    setup

---
+++ app/collections/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+    return <div>Collections</div>;
+};
+
+export default page;
---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import React, { useState } from "react";
+import Container from "../Container";
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const setOpen = () => {
+        setTimeout(() => {
+            setIsOpen(!isOpen);
+        });
+    };
+
+    return (
+        <>
+            <div
+                className={`${
+                    isOpen ? "block md:hidden" : "hidden"
+                } fixed z-50 bg-neutral-800/70 inset-0 `}
+            >
+                <div
+                    className={`
+                    translate
+            duration-300
+            h-full
+            ${
+                isOpen
+                    ? "translate-y-0 opacity-100"
+                    : "translate-y-full opacity-0"
+            }
+            
+                    
+        w-4/6 pt-20 pl-8 gap-4 h-full bg-white flex flex-col text-xl font-semibold relative
+        
+
+        `}
+                >
+                    <Image
+                        onClick={setOpen}
+                        className=" absolute top-4 left-4"
+                        src="/icon-close.svg"
+                        alt="Image"
+                        width="12"
+                        height="12"
+                    />
+                    <Link href="/collections">Collections</Link>
+                    <Link href="/men">Men</Link>
+                    <Link href="/women">Women</Link>
+                    <Link href="/about">About</Link>
+                    <Link href="/contact">Contact</Link>
+                </div>
+            </div>
+            <Container>
+                <div className="relative flex items-center justify-between py-8">
+                    <div className="flex items-center">
+                        <div className="flex gap-3 items-center">
+                            <Image
+                                onClick={setOpen}
+                                className="md:hidden"
+                                src="/icon-menu.svg"
+                                alt="Image"
+                                width="15"
+                                height="15"
+                            />
+
+                            <Link
+                                className="text-3xl font-bold pb-1.5"
+                                href="/"
+                            >
+                                sneakers
+                            </Link>
+                        </div>
+
+                        <div className={`ml-20 md:flex gap-5 hidden`}>
+                            <Link href="/collections">Collections</Link>
+                            <Link href="/men">Men</Link>
+                            <Link href="/women">Women</Link>
+                            <Link href="/about">About</Link>
+                            <Link href="/contact">Contact</Link>
+                        </div>
+                    </div>
+                    <div className="flex gap-12 items-center ">
+                        <div>
+                            <Image
+                                src="/icon-cart.svg"
+                                width="18"
+                                height="18"
+                                alt=""
+                            />
+                        </div>
+
+                        <Image
+                            src="/images/image-avatar.png"
+                            width="40"
+                            height="40"
+                            alt=""
+                        />
+                    </div>
+                </div>
+                <hr />
+            </Container>
+        </>
+    );
+};
+
+export default Navbar;
